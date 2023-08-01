39b80c912509fdc692dc093aef037551fa7f3330
clemlak <clementlakhal@gmail.com>
2023-08-01 14:30:35 +0400

    test: add predictAddress and related test

--- test/WrapperFactory.t.sol
+++ test/WrapperFactory.t.sol
@@ -55,4 +55,39 @@ contract WrapperFactoryTest is Test, ERC1155TokenReceiver {
         asset.approve(address(portfolio), type(uint256).max);
         quote.approve(address(portfolio), type(uint256).max);
     }
+
+    function test_deploy_returns_wrapper_address() public {
+        address wrapper = factory.deploy(poolId);
+        assertEq(wrapper, predictAddress());
+    }
+
+    function predictAddress() public view returns (address) {
+        string memory name =
+            string.concat("Wrapped Portfolio ", "Asset", " - ", "Quote");
+
+        string memory symbol = string.concat("wP", "ASSET", "-", "QUOTE");
+
+        bytes32 salt = keccak256(abi.encodePacked(asset, quote, poolId));
+        return address(
+            uint160(
+                uint256(
+                    keccak256(
+                        abi.encodePacked(
+                            bytes1(0xff),
+                            address(factory),
+                            salt,
+                            keccak256(
+                                abi.encodePacked(
+                                    type(ERC20Wrapper).creationCode,
+                                    abi.encode(
+                                        address(portfolio), poolId, name, symbol
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        );
+    }
 }
